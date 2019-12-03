3448784d523a5892ab9041ef784cf2c3f3daf83e
Lewis Henson <lewishenson@users.noreply.github.com>
2019-12-03 21:12:14 +0000

    Add day 1 puzzle 1 solution.

---
+++ src/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AdventOfCode2019
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var puzzle = new Puzzles.Day01.Puzzle1();
+            var answer = puzzle.Solve();
+            Console.WriteLine(answer);
+        }
+    }
+}
---
+++ src/Puzzles/Day01/Puzzle1.cs
@@ -0,0 +1,24 @@
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2019.Puzzles.Day01
+{
+    public class Puzzle1
+    {
+        public int Solve()
+        {
+            var masses = File.ReadAllLines("Puzzles\\Day01\\input.txt");
+
+            var totalFuel = masses.Select(int.Parse)
+                                  .Select(CalculateFuel)
+                                  .Sum();
+
+            return totalFuel;
+        }
+
+        private int CalculateFuel(int mass)
+        {
+            return (mass / 3) - 2;
+        }
+    }
+}
