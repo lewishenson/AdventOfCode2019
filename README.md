a80b259a0581953232cf327e707810336d2acc3a
Lewis Henson <lewishenson@users.noreply.github.com>
2019-12-04 23:05:04 +0000

    Add day 4 puzzle 2 solution.

--- README.md
+++ README.md
@@ -8,7 +8,28 @@ My solutions to the [Advent of Code 2019](https://adventofcode.com/2019).
 | 1   | Solved   | Solved   |
 | 2   | Solved   | Solved   |
 | 3   | Solved   | Solved   |
-| 4   | Solved   |          |
+| 4   | Solved   | Solved   |
+| 5   |          |          |
+| 6   |          |          |
+| 7   |          |          |
+| 8   |          |          |
+| 9   |          |          |
+| 10  |          |          |
+| 11  |          |          |
+| 12  |          |          |
+| 13  |          |          |
+| 14  |          |          |
+| 15  |          |          |
+| 16  |          |          |
+| 17  |          |          |
+| 18  |          |          |
+| 19  |          |          |
+| 20  |          |          |
+| 21  |          |          |
+| 22  |          |          |
+| 23  |          |          |
+| 24  |          |          |
+| 25  |          |          |
 
 ## To Run
 I've chosen to solve the puzzles using C# so make sure you've got .NET Core installed and just run the Console application.
---
+++ src/Puzzles/Day04/Puzzle2.cs
@@ -0,0 +1,68 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2019.Puzzles.Day04
+{
+    public class Puzzle2 : IPuzzle
+    {
+        public int Solve()
+        {
+            var (minimum, maximum) = GetInput();
+
+            var validPasswords = new List<int>();
+
+            for (var password = minimum; password <= maximum; password++)
+            {
+                if (IsPotentialPassword(password))
+                {
+                    validPasswords.Add(password);
+                }
+            }
+
+            return validPasswords.Count;
+        }
+
+        private (int minimum, int maximum) GetInput()
+        {
+            var input = File.ReadAllText("Puzzles\\Day04\\input.txt");
+
+            var numbers = input.Split('-')
+                               .Select(int.Parse)
+                               .ToList();
+
+            return (minimum: numbers[0], maximum: numbers[1]);
+        }
+
+        private bool IsPotentialPassword(int password)
+        {
+            if (!DigitsNeverDecrease(password))
+            {
+                return false;
+            }
+
+            if (!HasTwoAdjacentDigitsTheSame(password))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool DigitsNeverDecrease(int password)
+        {
+            var orderedDigits = password.ToString().OrderBy(c => c).ToArray();
+            var orderedPasswordString = new string(orderedDigits);
+            var orderedPassword = int.Parse(orderedPasswordString);
+
+            return orderedPassword == password;
+        }
+
+        private bool HasTwoAdjacentDigitsTheSame(int password)
+        {
+            var groupedDigits = password.ToString().GroupBy(c => c);
+
+            return groupedDigits.Any(group => group.Count() == 2);
+        }
+    }
+}
