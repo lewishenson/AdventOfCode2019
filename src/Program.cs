2883ea51cdb798ccc225f16b31cfeed6503e1e93
lewishenson <lewis.henson@gmail.com>
2019-12-08 08:24:30 +0000

    Add day 8 puzzle 1 solution.

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Reflection;
-using AdventOfCode2019.Puzzles;
 
 namespace AdventOfCode2019
 {
---
+++ src/Puzzles/Day08/Puzzle1.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2019.Puzzles.Day08
+{
+    public class Puzzle1 : IPuzzle
+    {
+        public int Solve()
+        {
+            var imageData = File.ReadAllText("Puzzles\\Day08\\input.txt").Trim();
+
+            const int width = 25;
+            const int height = 6;
+            const int size = width * height;
+
+            var layers = GetLayers(imageData, size).ToList();
+
+            var layerWithFewestZeroes = layers.OrderBy(layer => layer.Count(c => c == '0')).First();
+
+            var oneCount = layerWithFewestZeroes.Count(c => c == '1');
+            var twoCount = layerWithFewestZeroes.Count(c => c == '2');
+            var answer = twoCount * oneCount;
+
+            return answer;
+        }
+
+        private IEnumerable<string> GetLayers(string imageData, int size)
+        {
+            for (var i = 0; i < imageData.Length; i += size)
+            {
+                yield return imageData.Substring(i, Math.Min(size, imageData.Length - i));
+            }
+        }
+    }
+}
